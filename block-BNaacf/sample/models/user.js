0c742855bced6744b00a266a223a81b27d63e2b2
abc <joel.rajesh13@gmail.com>
2021-06-16 21:57:16 +0530

    Schema Types Mongoose

---
+++ block-BNaacf/sample/models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var ObjectId = Schema.Types.ObjectId
+var addressSchema = new Schema({
+    village:String,
+    city:String,
+    state:String,
+    pin:Number,
+    user:ObjectId
+})
