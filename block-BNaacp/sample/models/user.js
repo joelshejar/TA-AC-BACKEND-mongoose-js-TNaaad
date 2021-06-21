17e0c509e47dec861049b8d0992f974b92fa04b4
abc <joel.rajesh13@gmail.com>
2021-06-21 11:28:40 +0530

    Assignment 2

--- block-BNaacp/sample/models/user.js
+++ block-BNaacp/sample/models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 var userSchema = new Schema({
     name:String,
-    sports:String,
+    sports:[String],
     email:String,
 }, {timestamps: true})
 var User = mongoose.model('User',userSchema)
